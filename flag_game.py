ade67ae97577ab38f6100583554aa1488b1579b1
Roey <roitamano@gmail.com>
2024-09-10 16:50:41 +0300

    Initial commit

--- flag_game.py
+++ flag_game.py
@@ -5,7 +5,7 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((1400, 700))
 pygame.display.set_caption("moving with arrows")
 img = pygame.image.load(image_filename)
 img = pygame.transform.scale(img, (500, 500))
@@ -15,29 +15,38 @@ x = 0
 y = 0
 x1 = 50
 y1 = 50
+board = [[0 for i in range(50)] for j in range(25)]
+board[0][0] = 1
+for i in board:
+    print(i)
+block = 980000 / 1250
+
 while True:
     screen.fill((255, 255, 255))
-    screen.blit(img, (x, y))
-    screen.blit(img1, (x1, y1))
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT]:
-        x = x + 5
-    if keys[pygame.K_LEFT]:
-        x = x - 5
-    if keys[pygame.K_UP]:
-        y = y - 5
-    if keys[pygame.K_DOWN]:
-        y = y + 5
-    if keys[pygame.K_w]:
-        y1 = y1 - 5
-    if keys[pygame.K_s]:
-        y1 = y1 + 5
-    if keys[pygame.K_a]:
-        x1 = x1 - 5
-    if keys[pygame.K_d]:
-        x1 = x1 + 5
-    pygame.display.update()
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 1:
+                    screen.blit(img1, (i * (700 / 25), j * (1400 / 50)))
+        keys = pygame.key.get_pressed()
+        if event.type == pygame.KEYDOWN:
+            if keys[pygame.K_RIGHT]:
+                x = x + 1
+                board[0][x - 1] = 0
+                board[0][x] = 1
+                for i in board:
+                    print(i)
+                print("-----------------")
+            if keys[pygame.K_LEFT]:
+                x = x - 1
+                board[0][x + 1] = 0
+                board[0][x] = 1
+            if keys[pygame.K_UP]:
+                y = y - 5
+            if keys[pygame.K_DOWN]:
+                y = y + 5
+            pygame.display.update()
