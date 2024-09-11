6f031797af8fc8da489c0c82f2d0dab236b7bc67
Roey <roitamano@gmail.com>
2024-09-11 10:10:32 +0300

    Initial commit

--- flag_game.py
+++ flag_game.py
@@ -1,52 +1,84 @@
-image_filename = 'pixil-frame-0.png'
-image_filename1 = 'dog.png'
 import pygame
-
 from sys import exit
+import random
+
+image_filename1 = 'dog.png'
+image_filename2 = 'bush.png'
 
 pygame.init()
 screen = pygame.display.set_mode((1400, 700))
 pygame.display.set_caption("moving with arrows")
-img = pygame.image.load(image_filename)
-img = pygame.transform.scale(img, (500, 500))
+
 img1 = pygame.image.load(image_filename1)
-img1 = pygame.transform.scale(img1, (200, 200))
+img1 = pygame.transform.scale(img1, (2 * (700 / 25), 4 * (1400 / 50)))
+img2 = pygame.image.load(image_filename2)
+img2 = pygame.transform.scale(img2, (2 * (700 / 25), 2 * (1400 / 50)))
 x = 0
 y = 0
-x1 = 50
-y1 = 50
+
 board = [[0 for i in range(50)] for j in range(25)]
+for i in range(20):
+    random_row = random.randint(0, 24)
+    random_col = random.randint(0, 49)
+    if board[random_row][random_col] == 0:
+        board[random_row][random_col] = 2
 board[0][0] = 1
+
 for i in board:
     print(i)
-block = 980000 / 1250
 
 while True:
-    screen.fill((255, 255, 255))
-
+    screen.fill("dark green")
+    for i in range(25):
+        for j in range(50):
+            if board[i][j] == 2:
+                screen.blit(img2, (j * (1400 / 50), i * (700 / 25)))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == 1:
-                    screen.blit(img1, (i * (700 / 25), j * (1400 / 50)))
+
+        screen.blit(img1, (x * (1400 / 50), y * (700 / 25)))
         keys = pygame.key.get_pressed()
-        if event.type == pygame.KEYDOWN:
-            if keys[pygame.K_RIGHT]:
-                x = x + 1
-                board[0][x - 1] = 0
-                board[0][x] = 1
-                for i in board:
-                    print(i)
-                print("-----------------")
-            if keys[pygame.K_LEFT]:
-                x = x - 1
-                board[0][x + 1] = 0
-                board[0][x] = 1
-            if keys[pygame.K_UP]:
-                y = y - 5
-            if keys[pygame.K_DOWN]:
-                y = y + 5
-            pygame.display.update()
+
+
+        if keys[pygame.K_RIGHT]:
+            board[y][x] = 0
+            x += 1
+            if x > 48:
+                x = 48
+            board[y][x] = 1
+            for i in board:
+                print(i)
+            print("-----")
+        if keys[pygame.K_LEFT]:
+            board[y][x] = 0
+            x -= 1
+            if x < 0:
+                x = 0
+            board[y][x] = 1
+            for i in board:
+                print(i)
+            print("-----")
+        if keys[pygame.K_UP]:
+            board[y][x] = 0
+            y -= 1
+            if y < 0:
+                y = 0
+            board[y][x] = 1
+            for i in board:
+                print(i)
+            print("-----")
+        if keys[pygame.K_DOWN]:
+            board[y][x] = 0
+            y += 1
+            if y > 21:
+                y = 21
+            board[y][x] = 1
+            for i in board:
+                print(i)
+            print("-----")
+
+        pygame.display.update()
+
+
